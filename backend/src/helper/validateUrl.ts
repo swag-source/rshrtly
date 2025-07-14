632dd74172361e72cdb8691eddc9258a47cd263e
rama_seltzer <ramiroseltzer@gmail.com>
2025-07-14 15:03:36 -0300

    fixed valid url checker

--- backend/src/helper/validateUrl.ts
+++ backend/src/helper/validateUrl.ts
@@ -1,8 +1,14 @@
-export function isValidUrl(link: string): boolean {
-    try {
-        const url = new URL(link);
-        return url.protocol === 'http:' || url.protocol === 'https:';
-    } catch {
-        return false;
-    }
+export function isValidUrl(str : string) : boolean {
+    // Weird regex check for valid url's.
+    // Regex: 
+    const pattern = new RegExp(
+    '^([a-zA-Z]+:\\/\\/)?' + // protocol
+      '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
+      '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR IP (v4) address
+      '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
+      '(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
+      '(\\#[-a-z\\d_]*)?$', // fragment locator
+    'i');
+    
+    return pattern.test(str);
 }
---
+++ backend/src/testEnvironment.ts
@@ -0,0 +1,19 @@
+import { isValidUrl } from "./helper/validateUrl";
+
+console.log("Is valid : ", isValidUrl(""));
+console.log("Is valid : ", isValidUrl("google.com"));
+console.log("Is valid : ", isValidUrl("javascript:alert(1)"));
+console.log("Is valid : ", isValidUrl("https:///path"));
+console.log("Is valid : ", isValidUrl('https://a.co'));
+console.log("Is valid : ", isValidUrl('https://example-site.com'));
+console.log("Is valid : ", isValidUrl('facebook.com'));
+console.log("Is valid : ", isValidUrl(""));
+console.log("Is valid : ", isValidUrl(""));
+console.log("Is valid : ", isValidUrl(""));
+console.log("Is valid : ", isValidUrl(""));
+console.log("Is valid : ", isValidUrl(""));
+console.log("Is valid : ", isValidUrl(""));
+
+
+
+
